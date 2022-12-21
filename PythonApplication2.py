5ac8d1309f5c4d914afa642b796665ebda00e196
minhtuan1210 <121111562+minhtuan1210@users.noreply.github.com>
2022-12-21 23:10:25 +0700

    Update PythonApplication2.py

--- PythonApplication2.py
+++ PythonApplication2.py
@@ -7,7 +7,7 @@ CANVAS_HEIGHT = 480
 CANVAS_CENTER_X = CANVAS_WIDTH / 2  
 CANVAS_CENTER_Y = CANVAS_HEIGHT / 2  
 IMAGE_ENLARGE = 11  
-HEART_COLOR = "#f76070"  #chỉnh màu ở đây
+HEART_COLOR = "#ffffff"  #chỉnh màu ở đây
 
 def heart_function(t, shrink_ratio: float = IMAGE_ENLARGE):
 
